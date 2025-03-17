0b06dc5e11fce112dd5b4c8532e294692aaf6cb4
andiez02 <ken1162004@gmail.com>
2025-03-17 18:25:53 +0700

    chore: change language to vietnames

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import engboostLogo from "../../assets/home/engboost-logo.png";
 
 function Footer() {
-  return <footer className="h-[var(--height-footer)] bg-blue-900"></footer>;
+  return (
+    <footer className="min-h-[var(--height-footer)] flex flex-col items-center justify-center bg-blue-950 text-white py-6">
+      {/* Logo and Slogan */}
+      <div className="flex gap-3 items-center">
+        <img src={engboostLogo} alt="EngBoost Logo" className="h-10" />
+        <span className="text-lg font-semibold">Fuel your Fluency</span>
+      </div>
+
+      {/* Copyright */}
+      <div className="mt-4 text-sm text-gray-400">
+        © {new Date().getFullYear()} EngBoost. All rights reserved.
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import engboostLogo from "../../assets/home/engboost-logo.png";
 import { routes } from "../../utils/constants";
 import { useLocation, useNavigate } from "react-router-dom";
-import { HomeIcon } from "@heroicons/react/24/solid";
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
   const location = useLocation();
+  const navigate = useNavigate();
+
+  const isHome = location.pathname === routes.HOME;
 
   useEffect(() => {
     const handleScroll = () => {
@@ -16,8 +18,6 @@ function Header() {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-
-  const navigate = useNavigate();
 
   const dashboardItem = [
     { path: routes.HOME, name: "Trang chủ" },
@@ -29,9 +29,9 @@ function Header() {
   return (
     <header
       className={`h-[var(--height-header)] fixed top-0 left-0 w-screen flex justify-around transition-colors duration-500 ease-in-out ${
-        isScrolled
-          ? "bg-blue-200 shadow-lg opacity-95 text-black"
-          : "bg-transparent text-white"
+        !isScrolled && isHome
+          ? "bg-transparent text-white"
+          : "bg-blue-200 shadow-lg opacity-95 text-black"
       }`}
     >
       <div>
@@ -41,7 +41,11 @@ function Header() {
         <ul className="flex items-center justify-evenly h-full">
           {dashboardItem.map((item) => {
             const isActive = location.pathname === item.path;
-
+            const textColor = isHome
+              ? isScrolled
+                ? "text-black"
+                : "text-white"
+              : "text-gray-800";
             return (
               <li
                 key={item.name}
@@ -49,17 +53,11 @@ function Header() {
                 className="relative cursor-pointer px-4 py-2"
               >
                 <div
-                  className={`text-center text-lg flex items-center justify-center transition-all duration-300
+                  className={`text-center text-lg flex items-center justify-center transition-all duration-300 ${textColor}
                   ${
                     isActive
-                      ? `${
-                          isScrolled ? "text-blue-600" : "text-white"
-                        } font-semibold after:absolute after:bottom-0 after:left-1/2  after:h-[3px] after:bg-current after:rounded-full after:transition-all after:duration-300 after:ease-in-out after:-translate-x-1/2 after:w-full`
-                      : `${
-                          isScrolled
-                            ? "text-gray-700 hover:text-blue-500"
-                            : "text-white hover:text-gray-300"
-                        } hover:after:absolute hover:after:bottom-0 hover:after:left-1/2 hover:after:h-[3px] hover:after:bg-current hover:after:rounded-full hover:after:transition-all hover:after:duration-300 hover:after:ease-in-out hover:after:-translate-x-1/2 hover:after:w-full`
+                      ? "font-semibold after:absolute after:bottom-0 after:left-1/2 after:h-[3px] after:bg-current after:rounded-full after:transition-all after:duration-300 after:ease-in-out after:-translate-x-1/2 after:w-full"
+                      : "hover:text-blue-500 hover:after:absolute hover:after:bottom-0 hover:after:left-1/2 hover:after:h-[3px] hover:after:bg-current hover:after:rounded-full hover:after:transition-all hover:after:duration-300 hover:after:ease-in-out hover:after:-translate-x-1/2 hover:after:w-full"
                   }`}
                 >
                   <div>{item.name}</div>
@@ -70,10 +68,10 @@ function Header() {
         </ul>
       </div>
 
-      <div className="flex gap-4 items-center justify-evenly h-full group-hover:bg-blue-500">
+      <div className="flex gap-4 items-center justify-evenly h-full">
         <button
           className={`h-[60%] px-4 rounded-xl cursor-pointer shadow-md transition-all hover:translate-y-1 hover:shadow-sm ${
-            isScrolled
+            isScrolled || !isHome
               ? "bg-white text-black"
               : "bg-transparent border border-white text-white"
           }`}
@@ -83,7 +81,9 @@ function Header() {
 
         <button
           className={`h-[60%] px-4 rounded-xl cursor-pointer shadow-md transition-all hover:translate-y-1 hover:shadow-sm ${
-            isScrolled ? "bg-blue-500 text-white" : "bg-white text-black"
+            isScrolled || !isHome
+              ? "bg-blue-500 text-white"
+              : "bg-white text-black"
           }`}
         >
           Bắt đầu
